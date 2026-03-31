63656dc773139dd584443c4edeb292ff7e738495
“Nilde <nilde.benedetti@gmail.com>
2026-03-31 15:47:21 +0200

    creo variabili const per numero utente e numero cpu

--- script/script.js
+++ script/script.js
@@ -1,11 +1,27 @@
-// CHECK # 1 
-// Controlla se una parola e' un PALINDROMO
+// CHECK # 1 - Controlla se una parola e' un PALINDROMO
 
-
+/*=== SCOMMENTA PER CHECK ==============================
 const parolaTest = 'anna';
 
 
 const isItPalindromo = palindromoChecker(parolaTest);
 console.log(isItPalindromo);
+=======================================================*/
+
+// CHECK # 2 - Stabiliamo se la somma dei due numeri è pari o dispari
+
+/***Pari e Dispari**
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+const numeroPlayer = prompt('Inserisci un numero da 1 a 5','2');
+const numeroCpu = myRandom(1,5);
+
+console.log(numeroPlayer);
+console.log(numeroCpu);
+
 
 
